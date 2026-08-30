97427871d4769fc6dea9bc9dcf41055c1442cd3c
axel-lindberg <axelahus@gmail.com>
2026-08-30 09:48:21 +0200

    funktionen bit() och newarray()

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, left=none, right=none, maxinsubtree=-1):
+    def __init__(self, left=None, right=None, maxinsubtree=-1):
         self.left = left
         self.right = right
         self. maxinsubtree = maxinsubtree
@@ -8,3 +8,17 @@ class PersistentArray:
     def __init__(self, root=None, height=0):
         self.root = root
         self.height = height
+
+# tar in nodens riktiga nivå (inte nivå - 1 som i labb instruktionen utan det görs i funktionen)
+def bit(index: int, level: int) -> int: 
+    mask = 1 << (level - 1)
+    bit = (index & mask) >> (level - 1)
+    return bit
+        
+def newarray() -> PersistentArray:
+    return PersistentArray(None, 0)
+
+    
+if __name__ == "__main__":
+    i = bit(5, 1)
+    print(i)
