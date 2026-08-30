c74955454e6aade993b5f7686bb016b44dd7a562
axel-lindberg <axelahus@gmail.com>
2026-08-30 09:10:39 +0200

    main.py and initiall classes

---
+++ main.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, left=none, right=none, maxinsubtree=-1):
+        self.left = left
+        self.right = right
+        self. maxinsubtree = maxinsubtree
+    
+class PersistentArray:
+    def __init__(self, root=None, height=0):
+        self.root = root
+        self.height = height
